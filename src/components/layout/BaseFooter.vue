de514748f9a839ad776c02e3bf98348e505f283d
matteogaillard <matteo.gaillard@protonmail.com>
2024-07-20 17:51:22 +0200

    finished first page (with no feature)

--- src/components/layout/BaseFooter.vue
+++ src/components/layout/BaseFooter.vue
@@ -1,32 +1,54 @@
 <template>
   <footer>
-    <div>
+    <div class="top">
       <div>
-        <label for="email">{{ $t("footer.top.sendMailLabel") }}:</label>
-        <input id="email" type="email" />
-        <button class="icon submit"></button>
+        <label for="email">{{ $t("footer.top.sendMailLabel") }}</label>
+        <input id="email" type="email" placeholder="E-mail" />
+        <button class="icon"></button>
       </div>
       <div>
         <span>{{ $t("footer.top.changeLangLabel") }}:</span>
-        <div class="selector">
-          <span class="value">Français</span>
+        <div @click="toggleDropdown" class="selector">
+          <span class="value">{{translateLang(locale)}}</span>
           <span class="icon choose" />
+
+          <ul class="dropdown" v-show="langDropdownToggled">
+            <li @click="locale = lang" v-for="lang in availableLocales" :key="lang"  :class="{'selected': lang === locale}" class="icon">
+              {{translateLang(lang)}}
+            </li>
+          </ul>
         </div>
       </div>
     </div>
-    <div>
+    <div class="bottom">
       <h5>We build together - {{new Date().getFullYear()}} - {{$t("footer.allRightReserved")}}</h5>
       <div>
         <h5>{{$t("footer.followUsOn")}}</h5>
         <nav>
-          <a><span class="icon twitter" /></a>
-          <a><span class="icon instagram" /></a>
-          <a><span class="icon youtube" /></a>
+          <a href="/"><span class="icon twitter" /></a>
+          <a href="/"><span class="icon instagram" /></a>
+          <a href="/"><span class="icon youtube" /></a>
         </nav>
       </div>
     </div>
   </footer>
 </template>
+
+<script lang="ts" setup>
+import {ref} from "vue";
+import {useI18n} from "vue-i18n";
+
+const {locale, availableLocales, t} = useI18n();
+const langDropdownToggled = ref(false);
+
+function toggleDropdown() {
+  langDropdownToggled.value = !langDropdownToggled.value;
+}
+
+function translateLang(lang: string) {
+  return t(`locales.${lang}`);
+}
+</script>
 
 <style lang="scss" scoped>
 @import "@/scss/components/layout/base-footer.scss";
